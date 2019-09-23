a7ae8b79a0e477cc6083bce1f9d61846203bb36d
Jowie and Czyrlyn <jowie.jurado@gmail.com>
2019-09-23 01:26:34 -0700

    third commit

--- public/js/app.js
+++ public/js/app.js
@@ -53386,8 +53386,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\smart-home\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\smart-home\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! c:\xampp\htdocs\smart-home\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! c:\xampp\htdocs\smart-home\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
