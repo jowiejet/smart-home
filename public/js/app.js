c490494b264d1825af042f5e9d785b2be920e69b
unknown <jowie.jurado@gmail.com>
2019-09-25 23:11:02 -0700

    commit

--- public/js/app.js
+++ public/js/app.js
@@ -1913,6 +1913,11 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 /* harmony default export */ __webpack_exports__["default"] = ({
+  data: function data() {
+    return {
+      form: new Form({})
+    };
+  },
   mounted: function mounted() {
     console.log('Component mounted.');
   }
